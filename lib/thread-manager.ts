8205def49719a536d177fa58693cb54fa3dfb326
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-01-03 14:00:08 -0800

    update thread manager

--- lib/thread-manager.ts
+++ lib/thread-manager.ts
@@ -1,6 +1,6 @@
 import prismadb from "@/lib/prismadb";
 
-async function getThread(userId) {
+async function getThread(userId: string) {
     let thread = await prismadb.thread.findFirst({
         where: { userId },
         orderBy: { lastUpdated: 'desc' },
@@ -15,7 +15,7 @@ async function getThread(userId) {
     return thread.id;
 }
 
-async function saveThread(userId, threadId, messages) {
+async function saveThread(threadId: string, messages: any[]) {
     await prismadb.thread.update({
         where: { id: threadId },
         data: {
