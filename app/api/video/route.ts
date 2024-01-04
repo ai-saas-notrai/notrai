fc6f3398e397faec39c7d6f5fe0171ba0b49d9ac
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-01-03 16:14:34 -0800

    updates

--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -1,15 +1,17 @@
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from 'openai';
 
 import { checkSubscription } from "@/lib/subscription";
 import { incrementApiLimit, checkApiLimit } from "@/lib/api-limit";
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
 
-const openai = new OpenAIApi(configuration);
+
 
 export async function POST(
   req: Request
@@ -23,7 +25,7 @@ export async function POST(
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    if (!configuration.apiKey) {
+    if (!openai.apiKey) {
       return new NextResponse("OpenAI API Key not configured.", { status: 500 });
     }
 
@@ -38,18 +40,18 @@ export async function POST(
       return new NextResponse("Free trial has expired. Please upgrade to pro.", { status: 403 });
     }
 
-    const response = await openai.createChatCompletion({
-      model: "asst_VrmA6nyg3uzqLjetDBYr7kF1",
-      messages
+    const response = await  await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: messages
     });
 
     if (!isPro) {
       await incrementApiLimit();
     }
 
-    return NextResponse.json(response.data.choices[0].message);
+    return NextResponse.json(response.choices[0].message);
   } catch (error) {
-    console.log('[CONVERSATION_ERROR]', error);
+    console.log('[MUSIC_ERROR]', error);
     return new NextResponse("Internal Error", { status: 500 });
   }
 };
