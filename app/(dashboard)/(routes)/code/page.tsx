408b51468f00327d1e97224df4f83dc74dd57e2b
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-01-03 17:21:41 -0800

    code

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -116,29 +116,30 @@ const CodePage = () => {
             <Empty label="No conversation started." />
           )}
           <div className="flex flex-col-reverse gap-y-4">
-            {messages.map((message) => (
-              <div 
-                key={message.content} 
-                className={cn(
-                  "p-8 w-full flex items-start gap-x-8 rounded-lg",
-                  message.role === "user" ? "bg-white border border-black/10" : "bg-muted",
-                )}
-              >
-                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                <ReactMarkdown components={{
-                  pre: ({ node, ...props }) => (
-                    <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
-                      <pre {...props} />
-                    </div>
-                  ),
-                  code: ({ node, ...props }) => (
-                    <code className="bg-black/10 rounded-lg p-1" {...props} />
-                  )
-                }} className="text-sm overflow-hidden leading-7">
-                  {message.content || ""}
-                </ReactMarkdown>
-              </div>
-            ))}
+          {messages.map((message, index) => (
+            <div 
+              key={index} // Using the index as a key
+              className={cn(
+                "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                message.role === "user" ? "bg-white border border-black/10" : "bg-muted",
+              )}
+            >
+              {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+              <ReactMarkdown components={{
+                pre: ({ node, ...props }) => (
+                  <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                    <pre {...props} />
+                  </div>
+                ),
+                code: ({ node, ...props }) => (
+                  <code className="bg-black/10 rounded-lg p-1" {...props} />
+                )
+              }} className="text-sm overflow-hidden leading-7">
+                {message.content || ""}
+              </ReactMarkdown>
+            </div>
+          ))}
+
           </div>
         </div>
       </div>
