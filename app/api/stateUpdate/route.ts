70a3515a3ef45685fbb010c0679e3338b8f94324
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-01-22 18:37:31 -0800

    route update

--- app/api/stateUpdate/route.ts
+++ app/api/stateUpdate/route.ts
@@ -1,10 +1,8 @@
-// pages/api/updateUserState.ts
-
 import type { NextApiRequest, NextApiResponse } from 'next';
 import prismadb from '@/lib/prismadb'; // Adjust the import path as needed
+import { auth } from "@clerk/nextjs";
 
 type RequestBody = {
-  userId: string;
   fileID: string;
   state: string;
 };
@@ -20,27 +18,27 @@ export default async function handler(
 ) {
   if (req.method === 'POST') {
     try {
-      const { userId, fileID, state } = req.body as RequestBody;
+      const { userId } = auth();
+      const { fileID, state } = req.body as RequestBody;
+
+      if (!userId) {
+        throw new Error("User ID is not available");
+      }
 
       // Update the UserSubscription record with the new fileID and state
       const updatedUserSubscription = await prismadb.userSubscription.update({
-        where: {
-          userId: userId,
-        },
-        data: {
-          fileID: fileID,
-          state: state,
-        },
+        where: { userId },
+        data: { fileID, state },
       });
 
       res.status(200).json({ message: 'User state updated successfully', updatedUserSubscription });
     } catch (error) {
       console.error('Failed to update user state:', error);
-      res.status(500).send({ message: 'Internal Server Error' });
+      res.status(500).json({ message: 'Internal Server Error' });
     }
   } else {
     // Handle any non-POST requests
     res.setHeader('Allow', ['POST']);
-    res.status(405).end({ message: `Method ${req.method} Not Allowed` });
+    res.status(405).json({ message: `Method ${req.method} Not Allowed` });
   }
 }
