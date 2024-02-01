d469c1d232a57def21baebccf2d2fa8d2f83ab10
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-01 15:03:51 -0800

    rounded

--- components/quiz/StartCard.js
+++ components/quiz/StartCard.js
@@ -5,7 +5,7 @@ const StartCard = ({ handleState, handleTimerStart }) => {
   return (
     <div className="flex justify-center items-center h-screen bg-gray-50">
       {/* Card container with shadow, rounded borders, and increased internal padding */}
-      <div className="w-full max-w-md mx-auto bg-white shadow-lg rounded-2xl border border-gray-200 px-10 py-16 ">
+      <div className="w-full max-w-md mx-auto px-10 py-16 bg-white shadow-lg rounded-2xl border border-gray-200  ">
         <h1 className="text-2xl md:text-3xl font-bold text-gray-900 text-center mb-8">
           Coding Quiz Challenge
         </h1>
