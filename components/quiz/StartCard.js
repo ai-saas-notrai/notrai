afe1443632ae6d56848c4d4b7129acc446a8d6a6
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-01 15:00:03 -0800

    padding

--- components/quiz/StartCard.js
+++ components/quiz/StartCard.js
@@ -3,30 +3,26 @@ import { Button } from "@/components/ui/button";
 
 const StartCard = ({ handleState, handleTimerStart }) => {
   return (
-    <div className="flex flex-col justify-center items-center h-screen bg-gray-50">
-      <div className="mx-auto bg-white shadow-lg rounded-xl border border-gray-200" style={{ borderRadius: '30px' }}>
-        {/* Uniform padding around the content */}
-        <div className="flex flex-col justify-between h-full p-10 md:p-12"> 
-          <div>
-            <h1 className="text-2xl md:text-3xl font-bold text-gray-900 text-center mb-6">
-              Coding Quiz Challenge
-            </h1>
-            <p className="text-center text-md text-gray-700 font-medium mb-5">
-              Try to answer to following code-related questions within time limit.
-            </p>
-            <p className="text-center text-md text-gray-700 font-medium mb-6">
-              Keep in mind that incorrect answers will penalize your score/time by ten seconds!
-            </p>
-          </div>
-          <Button 
-            className="col-span-12 lg:col-span-2 w-full mt-8 mb-6" // Added margin-top for spacing and margin-bottom for padding from the bottom edge
-            type="submit" 
-            size="icon" 
-            onClick={() => { handleState("quiz"); handleTimerStart(); }}
-          >
-            Start Quiz
-          </Button>
-        </div>
+    <div className="flex justify-center items-center h-screen bg-gray-50">
+      {/* Card container with shadow, rounded borders, and increased internal padding */}
+      <div className="w-full max-w-md mx-auto bg-white shadow-lg rounded-2xl border border-gray-200 px-10 py-16 ">
+        <h1 className="text-2xl md:text-3xl font-bold text-gray-900 text-center mb-8">
+          Coding Quiz Challenge
+        </h1>
+        <p className="text-center text-md text-gray-700 font-medium mb-6">
+          Try to answer to following code-related questions within time limit.
+        </p>
+        <p className="text-center text-md text-gray-700 font-medium mb-8">
+          Keep in mind that incorrect answers will penalize your score/time by ten seconds!
+        </p>
+        <Button 
+          className="w-full py-2 text-lg" 
+          type="submit" 
+          size="large" 
+          onClick={() => { handleState("quiz"); handleTimerStart(); }}
+        >
+          Start Quiz
+        </Button>
       </div>
     </div>
   );
