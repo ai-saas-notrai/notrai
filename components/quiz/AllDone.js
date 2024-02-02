a6de62181aca4d30f01bb9061c5aac190f048798
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-01 16:27:08 -0800

    style

--- components/quiz/AllDone.js
+++ components/quiz/AllDone.js
@@ -4,49 +4,37 @@ const AllDone = ({ score, handleHighScore, handleState, handleReset }) => {
   const [name, setName] = useState("");
 
   return (
-    <div>
-      <div className="flex flex-col justify-center h-[90vh]">
-        <div className="lg:w-6/12 w-10/12 mx-auto px-10 py-16 outline outline-slate-300 outline-2 rounded-3xl shadow-xl">
-          <h1 className="font-black font-Inter mb-6 drop-shadow-sm">
-            All Done!
-          </h1>
-          <p className=" font-medium drop-shadow-sm mb-5">
-            Your Final Score is: {score}
-          </p>
-          <p className=" font-medium drop-shadow-sm mb-4">
-            Enter Initials<span className="text-red-500 font-medium text-lg">*</span>:
-            <input
-              className="sm:ml-3 mt-3 sm:mt-0 px-3 py-2 bg-white border shadow-sm border-gray-300 placeholder-gray-400 focus:outline-none focus:border-gray-700 focus:ring-gray-700 rounded-md sm:text-sm focus:ring-1 sm:w-[250px] w-[150px]"
-              placeholder="example: RS"
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              value={name}
-              required
-              id="submit"
-            />
-            {name ? (
-            <button
-              className="sm:ml-3 mt-3 sm:mt-0 bg-skin-main text-white px-3 py-2 rounded-lg hover:bg-green-900 transition-all shadow-md"
-              onClick={() => {
-                handleHighScore({name:name ,score:score});
-                handleState("highscore");
-                handleReset();
-              }}
-            >
-              Submit
-            </button>
-            ) : (
-              <button
-              className="sm:ml-3 mt-3 sm:mt-0 bg-gray-500 text-black px-3 py-2 rounded-lg transition-all shadow-md" type="submit" id="submit"
-              onClick={(e) => {
-              }}
-            >
-              Submit
-            </button>
-            )}
-          </p>
+    <div className="flex flex-col justify-center items-center h-screen bg-gray-50">
+      <div className="mx-auto px-8 py-16 md:px-12 md:py-20 bg-white shadow-lg rounded-xl border border-gray-200" style={{ borderRadius: '30px', paddingTop: "20px", paddingBottom: "20px" }}>
+        <h1 className="text-2xl md:text-3xl font-bold text-gray-900 text-center mb-6">
+          All Done!
+        </h1>
+        <p className="text-center text-md text-gray-700 font-medium mb-5">
+          Your Final Score is: {score}
+        </p>
+        <div className="text-center font-medium mb-4">
+          Enter Initials<span className="text-red-500">*</span>:
+          <input
+            className="block mx-auto mt-3 px-3 py-2 bg-white border shadow-sm border-gray-300 placeholder-gray-400 focus:outline-none focus:border-blue-500 focus:ring-blue-500 rounded-md text-sm w-full md:w-auto"
+            placeholder="example: JS"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            required
+          />
         </div>
+        <Button 
+          className="col-span-12 lg:col-span-2 w-full" 
+          type="submit" 
+          size="icon"
+          onClick={() => {
+            handleHighScore({ name: name, score: score });
+            handleState("highscore");
+            handleReset();
+          }}
+          disabled={!name} // Disable button if name is not entered
+        >
+          Submit
+        </Button>
       </div>
     </div>
   );
