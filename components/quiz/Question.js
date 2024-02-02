e901f236fcaf236160fa92c2d43b209ca430f7f5
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-02 09:33:39 -0800

    card syling

--- components/quiz/Question.js
+++ components/quiz/Question.js
@@ -23,25 +23,27 @@ const Question = ({
 
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-gray-50">
-      <div className="mx-auto px-8 py-16 md:px-12 md:py-20 bg-white shadow-lg rounded-xl border border-gray-200" style={{ borderRadius: '30px', paddingTop: "20px", paddingBottom: "20px" }}>
+      <div className="mx-auto px-8 py-16 md:px-12 md:py-20 bg-white shadow-lg rounded-xl border border-gray-200" style={{ borderRadius: '30px' }}>
         <h1 className="text-2xl md:text-3xl font-bold text-gray-900 text-center mb-6">
           {questionText}
         </h1>
-        {options.map((option, index) => (
-          <div
-            key={index}
-            // Increased margin for more space between options
-            className="my-6 p-3 border border-gray-300 shadow rounded-md flex justify-between items-center cursor-pointer hover:bg-gray-100"
-            onClick={() => handleAnswer(option)}
-          >
-            <span className="font-medium text-sm text-gray-700">{option}</span>
-          </div>
-        ))}
+        
+        <div className="text-center pt-2 space-y-2 text-zinc-900 font-medium">
+          {options.map((option, index) => (
+            <div
+              key={index}
+              className="p-3 border border-gray-300 shadow rounded-md flex justify-between items-center cursor-pointer hover:bg-gray-100 my-2"
+              onClick={() => handleAnswer(option)}
+            >
+              <span className="font-semibold text-sm text-gray-700">{option}</span>
+            </div>
+          ))}
+        </div>
         <br />
         <hr />
         <br />
         {isCorrect === null && (
-          <p className="font-medium drop-shadow-sm ml-3">
+          <p className="font-medium text-gray-700">
             Choose the best answer from the multiple choices.
           </p>
         )}
