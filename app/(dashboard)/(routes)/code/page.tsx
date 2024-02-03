e86fbdd4ba3f5abb97b3a2ae4192b4ecebbe1714
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-03 12:22:23 -0800

    timing

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -115,7 +115,8 @@ const QuizPage: React.FC = () => {
         bgColor="bg-violet-500/10"
       />
       <div className="fixed top-0 right-20 p-4 text-black">
-        <span>Time: {Math.floor(time / 1000)}s</span>
+      <span>Time: {Math.floor(time / 60000)}m {Math.floor((time % 60000) / 1000)}s</span>
+
       </div>
       <main className="flex-grow pt-5 p-4">
         <div className="flex flex-col min-h-screen">
