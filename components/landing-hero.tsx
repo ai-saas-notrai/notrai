b90c5d9ee2ea8f0e3f1a7e4b0340e7eedbdf7762
Wizzo <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-02 21:01:59 -0800

    Update landing-hero.tsx

--- components/landing-hero.tsx
+++ components/landing-hero.tsx
@@ -29,7 +29,7 @@ export const LandingHero = () => {
         </div>
       </div>
       <div className="text-sm md:text-xl font-light text-zinc-400">
-        Create content using AI 10x faster.
+        Master notary skills and knowledge 10x faster with AI.
       </div>
       <div>
         <Link href={isSignedIn ? "/dashboard" : "/sign-up"}>
