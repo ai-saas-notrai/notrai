b608e4914a1fa6dbbc74bda71c9c67fb1ffd64ce
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-05 19:59:24 -0800

    lander

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -56,20 +56,20 @@ export default function HomePage() {
           Become a California Notary Public with Notrai
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-        Your AI-Powered Copilot for a Smarter Path to Compliance and Expertise. Elevate Your Notary Career with Next-Level Assistance and Insights.
+        Your AI-Powered Copilot for a Smarter Path to Compliance and Expertise.
         </p>
 
 
-      <div className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm m-4">
+      <div className="rounded-lg border max-w-4xl p-8 px-4 md:px-8 focus-within:shadow-sm m-4">
         <h1>How to Become a California Notary Public</h1>
         <p>Step-by-step instructions on how to become a notary public in California.</p>
-        <p><strong>1. Notary Education: </strong>To become a California notary, you must first take a California notary course. New notaries in California must take a six-hour notary training course. Renewing notaries are required to take a three-hour notary course.</p>
+        <p style={{ marginBottom: '16px' }}><strong>1. Notary Education: </strong>To become a California notary, you must first take a California notary course. New notaries in California must take a six-hour notary training course. Renewing notaries are required to take a three-hour notary course.</p>
         
         <p style={{ marginBottom: '16px' }}><strong>2. California Notary Exam: </strong>All applicants seeking appointment as a California notary are required to pass the California notary exam prior to appointment as a notary public. You can register for the California notary exam <a href="https://www.cpshr.us/exam-registration/notary/registration/" style={{ textDecoration: "underline" }}>here</a>.</p>
         <p style={{ marginBottom: '16px' }}><strong>3. Receive Your Test Scores: </strong>You will receive a letter in the mail saying that you have passed the exam. Test results should be posted 8-10 business days after you take the exam. Click <a href="https://cmas.cpshr.us/CMAS/" style={{ textDecoration: "underline" }}>here</a> to view your California notary exam test results.</p>
         <p style={{ marginBottom: '16px' }}><strong>4. Submit Your California Live Scan: </strong>After you have passed the notary public exam, you must submit a <a href="http://www.sos.ca.gov/notary/checklist/fingerprints/" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "underline" }}>Live Scan</a> to the DOJ for your background check. The <a href="//cdn.notary.net/wp-content/uploads/2021/02/notary_livescan.pdf" style={{ textDecoration: "underline" }}>California Notary Public Live Scan Instructions</a> are included with your mailed test results. To schedule a Live Scan, go to <a href="https://oag.ca.gov/fingerprints/locations" style={{ textDecoration: "underline" }}>oag.ca.gov/fingerprints/locations</a>.</p>
-        <p style={{ marginBottom: '16px' }}><strong>5. Receive Your Commission Packet: </strong>Your commission packet should arrive 4-12 weeks from the exam date. The notary public commission packet will be mailed once the application has been approved and after the applicant has passed the background check. The California notary public commission packet includes:</p>
-        <ul className="blts">
+        <p ><strong>5. Receive Your Commission Packet: </strong>Your commission packet should arrive 4-12 weeks from the exam date. The notary public commission packet will be mailed once the application has been approved and after the applicant has passed the background check. The California notary public commission packet includes:</p>
+        <ul className="list-disc pl-5 space-y-2">
           <li>a cover letter with instructions;</li>
           <li>filing instructions;</li>
           <li>a notary public commission certificate;</li>
@@ -77,10 +77,11 @@ export default function HomePage() {
           <li>a Certificate of Authorization to Manufacture Notary Public Seals; and</li>
           <li>a list of Authorized Manufacturers of Notary Public Seals.</li>
         </ul>
+        <p style={{ marginBottom: '16px' }}></p>
         <p style={{ marginBottom: '16px' }}>If you took the exam at least six weeks prior to the expiration date on your current notary public commission, your new notary public commission will not be sent to you more than 30 days before the expiration date.</p>
         <p style={{ marginBottom: '16px' }}><strong>6. Order Your Notary Supplies:</strong>Order your notary supplies from an authorized supplier</p>
-        <p style={{ marginBottom: '16px' }}><strong>7. File Your California Notary Bond and Oath: </strong>You must file your notary bond and oath within 30 days of the start date of your notary commission. Bring the following items to your county clerk&apos;s office:</p>
-        <ul>
+        <p><strong>7. File Your California Notary Bond and Oath: </strong>You must file your notary bond and oath within 30 days of the start date of your notary commission. Bring the following items to your county clerk&apos;s office:</p>
+        <ul className="list-disc pl-5 space-y-2">
           <li>Original Bond</li>
           <li>Two Oath of Office Forms (these are included in your commission packet you received from the state)</li>
           <li>Original Commission Certificate</li>
