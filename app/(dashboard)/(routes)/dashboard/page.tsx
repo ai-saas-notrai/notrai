fa7f6e37cc4d7cb8d4a43a83361d1e2c7dc47101
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-05 20:02:35 -0800

    notarai

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -52,10 +52,10 @@ export default function HomePage() {
   return (
     <div>
       <div className="mb-8 space-y-4">
-        <h2 className="text-2xl md:text-4xl font-bold text-center">
+        <h2 className="text-2xl md:text-4xl font-bold text-center m-2">
           Become a California Notary Public with Notrai
         </h2>
-        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+        <p className="text-muted-foreground font-light text-sm md:text-lg text-center m-2">
         Your AI-Powered Copilot for a Smarter Path to Compliance and Expertise.
         </p>
 
