e27437396785e462bc035d39b8f2b1d2185f1779
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-05 17:09:48 -0800

    update model prisma

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -43,8 +43,8 @@ export async function GET() {
           price_data: {
             currency: "USD",
             product_data: {
-              name: "Genius Pro",
-              description: "Unlimited AI Generations"
+              name: "Notrai Pro",
+              description: "Unlimited Access"
             },
             unit_amount: 2000,
             recurring: {
---
+++ lib/question-limit.ts
@@ -0,0 +1,65 @@
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+import { MAX_FREE_COUNTS } from "@/constants";
+
+export const incrementQuestionLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return;
+  }
+
+  const userQuestionLimit = await prismadb.userQuestionLimit.findUnique({
+    where: { userId: userId },
+  });
+
+  if (userQuestionLimit) {
+    await prismadb.userQuestionLimit.update({
+      where: { userId: userId },
+      data: { count: userQuestionLimit.count + 1 },
+    });
+  } else {
+    await prismadb.userQuestionLimit.create({
+      data: { userId: userId, count: 1 },
+    });
+  }
+};
+
+export const checkQuestionLimit = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return false;
+  }
+
+  const userQuestionLimit = await prismadb.userQuestionLimit.findUnique({
+    where: { userId: userId },
+  });
+
+  if (!userQuestionLimit || userQuestionLimit.count < MAX_FREE_COUNTS) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const getQuestionLimitCount = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return 0;
+  }
+
+  const userQuestionLimit = await prismadb.userQuestionLimit.findUnique({
+    where: {
+      userId
+    }
+  });
+
+  if (!userQuestionLimit) {
+    return 0;
+  }
+
+  return userQuestionLimit.count;
+};
