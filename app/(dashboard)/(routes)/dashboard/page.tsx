cf117a6c69180f0b7eda7f56e48c14f51da55f9b
wizzo-gmb <91214699+wizzo-gmb@users.noreply.github.com>
2024-02-12 11:34:29 -0800

    routes

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,53 +1,6 @@
 "use client";
-import axios from "axios";
-import { useState } from 'react';
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/select';
-import { useRouter } from "next/navigation";
-import { Button } from "@/components/ui/button";
-import toast from 'react-hot-toast';
 
 export default function HomePage() {
-  const [selectedState, setSelectedState] = useState('');
-  const router = useRouter();
-
-  const handleStateChange = (newState: string) => {
-    setSelectedState(newState);
-    // Additional logic can be added here if needed
-  };
-
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleSubmit = async () => {
-    setIsLoading(true);
-    try {
-      const response = await axios.post('/api/updateUser', {
-        state: selectedState,
-        fileID: 'file_ID'
-      });
-      toast.success("State and file ID updated successfully!");
-    } catch (error) {
-      console.error("Failed to update state and file ID:", error);
-      toast.error("Failed to update state and file ID.");
-    } finally {
-      setIsLoading(false);
-      router.refresh();
-    }
-  };
-  
-  
-
-  const states = [
-     "California"
-  ];
-
-  // const states = [
-  //   "Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia",
-  //   "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland",
-  //   "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire",
-  //   "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
-  //   "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
-  //   "West Virginia", "Wisconsin", "Wyoming"
-  // ];
 
   return (
     <div>
@@ -93,27 +46,7 @@ export default function HomePage() {
 
     </div>
       </div>
-      <div className="px-4 md:px-20 lg:px-32 space-y-4">
-        <Select onValueChange={handleStateChange}>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Select State" />
-          </SelectTrigger>
-          <SelectContent>
-            {states.map((state) => (
-              <SelectItem key={state} value={state}>{state}</SelectItem>
-            ))}
-          </SelectContent>
-        </Select>
-        <Button 
-      onClick={handleSubmit} 
-      className="col-span-12 lg:col-span-2 w-full" 
-      type="submit" 
-      size="icon"
-      disabled={isLoading} // Disable button when loading
-    >
-      {isLoading ? 'Updating...' : 'Update State'}
-    </Button>
-      </div>
+      
     </div>
   );
 }
--- components/exam/TimeUp.js
+++ components/exam/TimeUp.js
@@ -6,10 +6,10 @@ const TimeUp = ({handleState}) => {
         <div className="flex flex-col justify-center items-center h-screen bg-gray-50">
           <div className="mx-auto px-8 py-16 md:px-12 md:py-20 bg-white shadow-lg rounded-xl border border-gray-200" style={{ borderRadius: '30px', paddingTop: "20px", paddingBottom: "20px" }}>
                 <h1 className="text-2xl md:text-3xl font-bold text-gray-900 text-center mb-6">
-                    Time Up!
+                    Time is Up!
                 </h1>
                 <p className="text-center text-md text-gray-700 font-medium mb-5">
-                    Your Score is recorded!
+                    You have completed the california 6 hour Notary preparation test. 
                 </p>
                 <p className="text-center text-md text-gray-700 font-medium mb-4">
                     Click on Check Score to check your final score and submit it!
